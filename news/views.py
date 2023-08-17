280ffbd66d813c185310d889eb5ca79223fc67fc
R2D2 <r2d2@MacBook-Pro-R2D2.local>
2023-08-18 00:37:41 +0600

    lesson 9 is next

--- news/views.py
+++ news/views.py
@@ -5,5 +5,5 @@ from .models import Articles
 
 
 def news_home(request):
-    news = Articles.objects.all()
+    news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
