066cac4030aa6bfbd0354a753cdc215626990ff9
R2D2 <r2d2@MacBook-Pro-R2D2.local>
2023-08-18 23:04:21 +0600

    the end

--- news/views.py
+++ news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(request):
@@ -13,6 +13,19 @@ class NewsDetailView(DetailView):
     model = Articles
     template_name = 'news/details_view.html'
     context_object_name = 'article'
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+
+    form_class = ArticlesForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    template_name = 'news/news_delete.html'
+    success_url = '/news/'
 
 
 def create(request):
